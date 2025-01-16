f56f0d61b8491ed5faa475fff4771ba7f7d07180
Guilherme Baralhas <guilhermebaralhas@gmail.com>
2025-01-16 18:15:15 -0300

    test

--- sistemaweb2/src/main/java/com/vsmanutencoes/sistemaweb/config/SecurityConfig.java
+++ sistemaweb2/src/main/java/com/vsmanutencoes/sistemaweb/config/SecurityConfig.java
@@ -3,12 +3,9 @@ package com.vsmanutencoes.sistemaweb.config;
 import com.vsmanutencoes.sistemaweb.service.UserDetailsServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 
 @Configuration
 public class SecurityConfig {
