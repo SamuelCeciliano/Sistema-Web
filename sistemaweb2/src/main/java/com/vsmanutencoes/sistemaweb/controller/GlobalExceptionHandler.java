46a830b99e396400b424af7281e127b49ea7597d
Samuel Ceciliano <sceciliano90@gmail.com>
2025-01-25 20:55:05 -0300

    Novo Aviso de erro para: Materiais, Serivicos e Equipamentos

--- sistemaweb2/src/main/java/com/vsmanutencoes/sistemaweb/controller/GlobalExceptionHandler.java
+++ sistemaweb2/src/main/java/com/vsmanutencoes/sistemaweb/controller/GlobalExceptionHandler.java
@@ -1,17 +1,19 @@
 package com.vsmanutencoes.sistemaweb.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.dao.DataIntegrityViolationException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(DataIntegrityViolationException.class)
-    public ResponseEntity<String> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
+    public String handleDataIntegrityViolationException(DataIntegrityViolationException ex, RedirectAttributes redirectAttributes) {
         String messageError = "Não é possível excluir este registro, pois ele possui vínculos em outros registros.";
-        return new ResponseEntity<>(messageError, HttpStatus.BAD_REQUEST);
+        redirectAttributes.addFlashAttribute("errorMessage", messageError);
+        return "redirect:/materiais"; // redireciona para a página de listagem
     }
 }
+
+
